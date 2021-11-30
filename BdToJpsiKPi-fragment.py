b4bbc363d686a05be4d4361daf94e306b0ea54fc
rraturi <rishabh.raturi@cern.ch>
2021-11-30 16:05:08 +0530

    first commit

---
+++ BdToJpsiKPi-fragment.py
@@ -0,0 +1,97 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+from GeneratorInterface.EvtGenInterface.EvtGenSetting_cff import *
+
+generator = cms.EDFilter(
+    "Pythia8GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(7.010e-01),
+    crossSection = cms.untracked.double(540000000.),
+    comEnergy = cms.double(13000.0),
+    ExternalDecays = cms.PSet(
+        EvtGen130 = cms.untracked.PSet(
+            decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2014_NOLONGLIFE.DEC'),
+            particle_property_file = cms.FileInPath('GeneratorInterface/EvtGenInterface/data/evt_2014.pdl'),
+            #user_decay_file = cms.vstring('GeneratorInterface/EvtGenInterface/data/Bd_JpsiKPi.dec'),
+            list_forced_decays = cms.vstring('MyB0', 'Myanti-B0'),
+            operates_on_particles = cms.vint32(511),
+            convertPythiaCodes = cms.untracked.bool(False),
+            user_decay_embedded = cms.vstring(
+"""
+#
+###########################################################
+# Descriptor: [B0 -> Jpsi (mu+ mu-)K+ pi-)] + cc #
+###########################################################
+Alias      MyB0        B0
+Alias      Myanti-B0   anti-B0
+ChargeConj MyB0        Myanti-B0
+Alias      MyJ/psi  J/psi
+ChargeConj MyJ/psi  MyJ/psi
+#
+Decay MyB0
+  1.000    MyJ/psi       pi-   K+             PHSP;
+Enddecay
+CDecay Myanti-B0
+#
+Decay MyJ/psi
+  1.000         mu+       mu-            PHOTOS VLL;
+Enddecay
+#
+End
+"""
+            ),
+        ),
+        parameterSets = cms.vstring('EvtGen130')
+    ),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(
+            "SoftQCD:nonDiffractive = on",
+            'PTFilter:filter = on', # this turn on the filter
+            'PTFilter:quarkToFilter = 5', # PDG id of q quark
+            'PTFilter:scaleToFilter = 1.0'),
+        parameterSets = cms.vstring(
+            'pythia8CommonSettings',
+            'pythia8CUEP8M1Settings',
+            'processParameters',
+        )
+    )
+)
+
+bfilter = cms.EDFilter(
+    "PythiaFilter",
+    MaxEta = cms.untracked.double(9999.),
+    MinEta = cms.untracked.double(-9999.),
+    ParticleID = cms.untracked.int32(511)
+)
+
+decayfilter = cms.EDFilter(
+    "PythiaDauVFilter",
+    verbose         = cms.untracked.int32(1),
+    NumberDaughters = cms.untracked.int32(3),
+    ParticleID      = cms.untracked.int32(511),
+    DaughterIDs     = cms.untracked.vint32(-211, 321, 443),  ## pi-, K+, JPSI
+    MinPt           = cms.untracked.vdouble(0.4, 0.4, -1.),
+    MinEta          = cms.untracked.vdouble(-2.5, -2.5, -9999.),
+    MaxEta          = cms.untracked.vdouble( 2.5,  2.5,  9999.)
+    )
+
+jpsifilter = cms.EDFilter(
+    "PythiaDauVFilter",
+    MotherID = cms.untracked.int32(511),
+    ParticleID = cms.untracked.int32(443),
+    NumberDaughters = cms.untracked.int32(2),
+    DaughterIDs = cms.untracked.vint32(13, -13),
+    MinPt = cms.untracked.vdouble(2.5, 2.5),
+    MinEta = cms.untracked.vdouble(-2.5, -2.5),
+    MaxEta = cms.untracked.vdouble(2.5, 2.5),
+    verbose = cms.untracked.int32(1)
+)
+
+
+ProductionFilterSequence = cms.Sequence(generator*bfilter*decayfilter*jpsifilter)
---
+++ BsToJpsiKK-fragment.py
@@ -0,0 +1,107 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+from GeneratorInterface.EvtGenInterface.EvtGenSetting_cff import *
+
+generator = cms.EDFilter(
+    "Pythia8GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(7.010e-01),
+    crossSection = cms.untracked.double(540000000.),
+    comEnergy = cms.double(13000.0),
+    ExternalDecays = cms.PSet(
+        EvtGen130 = cms.untracked.PSet(
+            decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2014_NOLONGLIFE.DEC'),
+            particle_property_file = cms.FileInPath('GeneratorInterface/EvtGenInterface/data/evt_2014.pdl'),
+            #user_decay_file = cms.vstring('GeneratorInterface/EvtGenInterface/data/Bs_JpsiKK_mumuKK.dec'),
+            list_forced_decays = cms.vstring('MyB_s0', 'Myanti-B_s0'),
+            operates_on_particles = cms.vint32(531),
+            convertPythiaCodes = cms.untracked.bool(False),
+            user_decay_embedded = cms.vstring(
+"""
+#
+# This is the decay file for the decay BS0 -> J/PSI(-> MU+ MU-) K+ K-
+#
+# Descriptor: [B_s0 -> (J/psi(1S) -> mu+ mu-) ( K+ K-)]
+#
+# NickName: Bs_Jpsi2K
+#
+# Physics: Currently using phase space for Bs decays
+#
+# Tested:
+# By: Niladribihari Sahoo
+# Date: 24 Dec 2018
+#
+Alias      MyB_s0   B_s0
+Alias      Myanti-B_s0   anti-B_s0
+ChargeConj Myanti-B_s0   MyB_s0 
+Alias      MyJ/psi  J/psi
+ChargeConj MyJ/psi  MyJ/psi
+#
+Decay MyB_s0
+  1.000         MyJ/psi     K+  K-      PHSP;
+Enddecay
+#
+CDecay Myanti-B_s0
+#
+Decay MyJ/psi
+  1.000         mu+         mu-           PHOTOS VLL;
+Enddecay
+#
+End
+"""
+            ),
+        ),
+        parameterSets = cms.vstring('EvtGen130')
+    ),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(
+            "SoftQCD:nonDiffractive = on",
+            'PTFilter:filter = on', # this turn on the filter
+            'PTFilter:quarkToFilter = 5', # PDG id of q quark
+            'PTFilter:scaleToFilter = 1.0'),
+        parameterSets = cms.vstring(
+            'pythia8CommonSettings',
+            'pythia8CUEP8M1Settings',
+            'processParameters',
+        )
+    )
+)
+
+bfilter = cms.EDFilter(
+    "PythiaFilter",
+    MaxEta = cms.untracked.double(9999.),
+    MinEta = cms.untracked.double(-9999.),
+    ParticleID = cms.untracked.int32(531)
+)
+
+decayfilter = cms.EDFilter(
+    "PythiaDauVFilter",
+    verbose         = cms.untracked.int32(1),
+    NumberDaughters = cms.untracked.int32(3),
+    ParticleID      = cms.untracked.int32(531),
+    DaughterIDs     = cms.untracked.vint32(-321, 321, 443),  ## K-, K+, JPSI
+    MinPt           = cms.untracked.vdouble(0.4, 0.4, -1.),
+    MinEta          = cms.untracked.vdouble(-2.5, -2.5, -9999.),
+    MaxEta          = cms.untracked.vdouble( 2.5,  2.5,  9999.)
+    )
+
+jpsifilter = cms.EDFilter(
+    "PythiaDauVFilter",
+    MotherID = cms.untracked.int32(531),
+    ParticleID = cms.untracked.int32(443),
+    NumberDaughters = cms.untracked.int32(2),
+    DaughterIDs = cms.untracked.vint32(13, -13),
+    MinPt = cms.untracked.vdouble(2.5, 2.5),
+    MinEta = cms.untracked.vdouble(-2.5, -2.5),
+    MaxEta = cms.untracked.vdouble(2.5, 2.5),
+    verbose = cms.untracked.int32(1)
+)
+
+
+ProductionFilterSequence = cms.Sequence(generator*bfilter*decayfilter*jpsifilter)
